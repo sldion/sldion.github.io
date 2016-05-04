62753a10286fdb881a1cac695446f07e946e1c8b
Shawn Dion <shawndion@Shawns-MacBook-Air.local>
2016-05-04 08:54:49 -0400

    removed some useless files

--- datDotaData.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import csv as csv
-
-
-with open('Matches .csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print ', '.join(row)
